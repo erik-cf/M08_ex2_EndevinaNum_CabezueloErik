c468f481c025eebd95f69ae8acedc09793ce937a
Erik Cabezuelo <erikcf95@gmail.com>
2019-10-01 16:37:21 +0200

    Feta la primera Activity

--- app/src/main/java/com/example/endevinaelnumero/MainActivity.java
+++ app/src/main/java/com/example/endevinaelnumero/MainActivity.java
@@ -3,21 +3,54 @@ package com.example.endevinaelnumero;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    // Declarem els components que haurem de codificar.
+    EditText eTNumero;
+    Button b;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button b = findViewById(R.id.button);
+        final int numRandom = (int) ((Math.random() * 101) + 1);
+        eTNumero = findViewById(R.id.eTNumero);
+        b = findViewById(R.id.button);
         b.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                int numIntroduit = Integer.parseInt(eTNumero.getText().toString());
+                if(numIntroduit < 1 || numIntroduit > 100){
+                    Toast.makeText(MainActivity.this, "Has de ficar un número entre 1 i 100!", Toast.LENGTH_LONG).show();
+                    eTNumero.setText("");
+                }else {
+                    if (numIntroduit == numRandom) {
 
-
+                    } else if (numIntroduit < numRandom) {
+                        Toast.makeText(MainActivity.this, "El número que busquem es més gran que el que has introduit.", Toast.LENGTH_LONG).show();
+                        eTNumero.setText("");
+                    } else {
+                        Toast.makeText(MainActivity.this, "El número que busquem es més petit que el que has introduit.", Toast.LENGTH_LONG).show();
+                        eTNumero.setText("");
+                    }
+                }
             }
         });
+
+        eTNumero.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)){
+                    b.callOnClick();
+                }
+                return false;
+            }
+        });
+
     }
 }
