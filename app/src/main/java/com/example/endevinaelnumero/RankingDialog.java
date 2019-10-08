e7d87fede36be945535dc1322506401ff5598e1d
Erik Cabezuelo <erikcf95@gmail.com>
2019-10-08 16:43:32 +0200

    Logica de boto positiu del dialog feta

--- app/src/main/java/com/example/endevinaelnumero/RankingDialog.java
+++ app/src/main/java/com/example/endevinaelnumero/RankingDialog.java
@@ -6,21 +6,36 @@ import androidx.fragment.app.DialogFragment;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 public class RankingDialog extends DialogFragment {
 
-    @Override
+    public EditText eT1;
+
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder dialeg = new AlertDialog.Builder(getActivity(), R.style.AlertDialogTheme);
+        final AlertDialog.Builder dialeg = new AlertDialog.Builder(getActivity(), R.style.AlertDialogTheme);
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.activity_ranking_dialog, null);
+        eT1 = view.findViewById(R.id.nomGuanyador);
+        dialeg.setView(view);
+        dialeg.setCancelable(false);
+        dialeg.setPositiveButton(R.string.accept, new DialogInterface.OnClickListener() {
 
-        LayoutInflater inflater = requireActivity().getLayoutInflater();
-
-        dialeg.setView(inflater.inflate(R.layout.activity_ranking_dialog, null)).setPositiveButton(R.string.accept, new DialogInterface.OnClickListener() {
-            @Override
             public void onClick(DialogInterface dialog, int which) {
-
+                Intent i = new Intent(getContext(), RankingActivity.class);
+                if(eT1.getText().equals("") ||eT1.getText() == null){
+                    i.putExtra("nomEscrit", false);
+                }else{
+                    i.putExtra("nomEscrit", true);
+                    i.putExtra("nomGuanyador", eT1.getText());
+                }
+                startActivity(i);
             }
         }).setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
             @Override
