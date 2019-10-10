337baf63805ae5125379e1eebfe6ee514f797984
Erik Cabezuelo <erikcf95@gmail.com>
2019-10-10 20:03:59 +0200

    Preparant la listView

--- app/src/main/java/com/example/endevinaelnumero/RankingActivity.java
+++ app/src/main/java/com/example/endevinaelnumero/RankingActivity.java
@@ -5,33 +5,71 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 
 public class RankingActivity extends AppCompatActivity {
+
+    ArrayList<Record> aLP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        aLP = new ArrayList<Record>();
         Bundle args = getIntent().getExtras();
         boolean nomEscrit = args.getBoolean("nomEscrit");
         if(nomEscrit){
             String nom = args.getString("nomGuanyador");
             int intentos = args.getInt("intentos");
             try {
-                escriuNomFitxer(nom, intentos);
+                escriuFitxer(nom, intentos);
             } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    private void escriuNomFitxer(String nom, int intentos) throws FileNotFoundException {
-        OutputStreamWriter osw = new OutputStreamWriter(openFileOutput("rankingPuntuacions", Context.MODE_APPEND));
+    private void escriuFitxer(String nom, int intentos) throws IOException {
+        if(existeixFitxer("rankingPuntuacions.txt")) {
+            OutputStreamWriter osw = new OutputStreamWriter(openFileOutput("rankingPuntuacions.txt", Context.MODE_APPEND));
+            osw.write(nom + "\r\n");
+            osw.write(intentos + "\r\n");
+            osw.close();
+        }
+    }
 
+    private void llegeixFitxer() throws IOException {
+        if(existeixFitxer("rankingPuntuacions.txt")) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(openFileInput("rankingPuntuacions.txt")));
+            String nom;
+            int intents;
+            Record r;
+            while (br.ready()) {
+                nom = br.readLine();
+                intents = Integer.parseInt(br.readLine());
+                r = new Record(nom, intents);
+                aLP.add(r);
+            }
+        }
+    }
+
+    private boolean existeixFitxer(String nomFitxer) throws IOException {
+        File f = new File("rankingPuntuacions.txt");
+        if(f.exists()) {
+            return true;
+        }else{
+            f.createNewFile();
+            return true;
+        }
     }
 
     public boolean onSupportNavigateUp() {
---
+++ app/src/main/java/com/example/endevinaelnumero/Record.java
@@ -0,0 +1,16 @@
+package com.example.endevinaelnumero;
+
+public class Record {
+    String nomGuanyador;
+    int intentos;
+
+    public Record(){
+
+    }
+
+    public Record(String nomGuanyador, int intentos){
+        this.nomGuanyador = nomGuanyador;
+        this.intentos = intentos;
+    }
+
+}
